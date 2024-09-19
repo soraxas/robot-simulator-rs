c56776dcde7761fc71a2e2d54c6a792302a60942
Tin Lai <tin@tinyiu.com>
2024-09-19 17:18:35 +1000

    track test script
    Signed-off-by: Tin Lai <tin@tinyiu.com>

---
+++ python/test_script.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+pip install . && exec ipython -c '
+import robotsim
+import numpy as np
+r = robotsim.Robot("../assets/panda/urdf/panda_relative.urdf")
+
+print(r.joints)
+
+joints = np.random.rand(55, 9)
+joints = np.zeros([55, 9])
+# r.has_collision(joints)
+
+r.has_collision([[0]*9]*5)
+
+limits = np.array(r.joint_limits)
+
+def get_random_joints():
+    return np.random.rand(len(limits)) * (limits[:, 1] - limits[:, 0]) + limits[:, 0]
+
+' -i
+
